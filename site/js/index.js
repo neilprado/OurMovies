1519b4db035c85fb35f0130419c1619104de39ff
neilprado <neiljohn17@gmail.com>
2018-02-23 07:25:22 -0300

    Add files via upload

---
+++ site/js/index.js
@@ -0,0 +1,18 @@
+
+import $ from 'jquery';
+window.$ = $;
+
+import 'bootstrap';
+ 
+import './fancybox.js'; 
+import './slick.js'; 
+
+import getMovieInfo from './main.js';
+window.getMovieInfo = getMovieInfo;
+
+
+
+
+
+
+
